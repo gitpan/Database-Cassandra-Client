c31065fb19c4e923beb2e7d29f6aa950f8e785f3
Alexander Borisov <lastmac@cpan.org>
2014-11-25 15:25:43 +0000

    Import of LASTMAC/Database-Cassandra-Client-0.52.tar.gz from CPAN.
    gitpan-cpan-distribution: Database-Cassandra-Client
    gitpan-cpan-version:      0.52
    gitpan-cpan-path:         LASTMAC/Database-Cassandra-Client-0.52.tar.gz
    gitpan-cpan-author:       LASTMAC
    gitpan-cpan-maturity:     released

--- Client.pm
+++ Client.pm
@@ -5,7 +5,7 @@ use strict;
 use vars qw($AUTOLOAD $VERSION $ABSTRACT @ISA @EXPORT);
 
 BEGIN {
-	$VERSION = 0.51;
+	$VERSION = 0.52;
 	$ABSTRACT = "Cassandra client (XS for libcassandra)";
 	
 	@ISA = qw(Exporter DynaLoader);
